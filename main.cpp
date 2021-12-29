4b0d88ca91a0780566e826884ed1c274c180745a
C.S. Melis <cees3331x00@gmail.com>
2021-12-29 17:42:05 +0100

    Fix bugs

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main() {
     buffer[3] = NULL;
 
     std::ifstream fileIn;
-    std::ofstream fileOut;
 
     // TODO do not use hardcoded path
     assert(std::filesystem::exists("C:\\test1.txt"));
@@ -23,23 +22,26 @@ int main() {
     rename(std::string("C:\\test1.txt").c_str(), newName.c_str());
     fileIn.open(newName, std::ios::binary | std::ios::out);
 
-    fileOut.open("C:\\test1.txt", std::ios::binary | std::ios::out);
-
     fileIn.read(buffer, 3);
 
     if (strcmp(buffer, bom) == 0) {
         std::cout << "BOM found!" << std::endl;
         fileIn.seekg(3);
 
+        std::ofstream fileOut;
+        fileOut.open("C:\\test1.txt", std::ios::binary | std::ios::out);
+
         for (char character; fileIn.get(character);) {
             fileOut.write(&character, 1);
         }
+
+        fileIn.close();
+        fileOut.close();
+        remove(newName.c_str());
+    } else {
+        fileIn.close();
+        rename(newName.c_str(), std::string("C:\\test1.txt").c_str());
     }
-
-    fileIn.close();
-    fileOut.close();
-
-    remove(newName.c_str());
 
     return 0;
 }
